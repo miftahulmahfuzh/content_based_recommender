6fde0fe31ff5a253b6ffb205ad8d0b3f271e42cb
miftahulmahfuzh <mahfuzh74@gmail.com>
2018-01-11 11:47:27 +0700

    first commit

---
+++ enginesblob.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import time
+import redis
+from flask import current_app
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+import tfidfblob
+
+
+def info(msg):
+    current_app.logger.info(msg)
+
+
+class ContentEngine(object):
+
+    SIMKEY = 'p:smlr:%s'
+
+    def __init__(self):
+        self._r = redis.StrictRedis.from_url(current_app.config['REDIS_URL'])
+
+    def train(self, data_source):
+        start = time.time()
+        ds = pd.read_csv(data_source)
+        info("Training data ingested in %s seconds." % (time.time() - start))
+
+        # Flush the stale training data from redis
+        self._r.flushdb()
+
+        start = time.time()
+        self._train(ds)
+        info("Engine trained in %s seconds." % (time.time() - start))
+
+    def _train(self, ds):
+        similar_items = tfidfblob.get_similar_articles(ds)
+        for idx, row in ds.iterrows():
+            self._r.zadd(self.SIMKEY % row['id'], *similar_items[idx])
+
+    def predict(self, item_id, num):
+        return self._r.zrange(self.SIMKEY % item_id, 0, num-1, withscores=True, desc=True) 
+
+content_engine = ContentEngine()
---
+++ tfidfblob.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, unicode_literals
+import math, operator
+from textblob import TextBlob
+from scipy import spatial
+
+def tf(word, blob):
+    return blob.words.count(word) / len(blob.words)
+
+def n_containing(word, bloblist):
+    return sum(1 for blob in bloblist if word in blob.words)
+
+def idf(word, bloblist):
+    return math.log(len(bloblist) / (1 + n_containing(word, bloblist)))
+
+def tfidf(word, blob, bloblist):
+    return tf(word, blob) * idf(word, bloblist) 
+
+def preprocess(bloblist, stopwords):
+    bloblist = [blob.lower() for blob in bloblist]
+    for blob in bloblist:
+        list_temp = [word for word in blob.words if word not in stopwords]
+        blob = TextBlob(' '.join(list_temp))
+    return bloblist
+
+stopwords = ['href', 'class', 'title', 'blue', 'div', 'http']
+
+def get_similar_articles(ds):    
+    content_list = [TextBlob(repr(ds['content'][idx])) for idx, row in ds.iterrows()]
+    # Remove stopwords from articles
+    bloblist = preprocess(content_list, stopwords)
+    # Get word frequency in each article using tfidf
+    scores_list = []
+    for i, blob in enumerate(bloblist):
+        scores = {word: tfidf(word, blob, bloblist) for word in blob.words}
+        sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+        scores_list.append(scores)
+    # Get similarity between articles using cosine similarity
+    distances_list = []
+    distances_list_tmp = []
+    for i,scores in enumerate(scores_list):
+        distances = {}
+        for j,scores_tmp in enumerate(scores_list[i:]):
+            distances[j+i+1] = 1 - spatial.distance.cosine(scores.values()[:13],scores_tmp.values()[:13]) 
+        distances.update({idx+1:ds.get(i+1) for idx,ds in enumerate(distances_list_tmp)})
+        distances_list_tmp.append(distances)    
+        distances_touple = sorted(distances.items(), key=operator.itemgetter(1), reverse=True)
+        distances_list.append([(y,x) for x,y in distances_touple])
+    return [sum(distances[1:],()) for distances in distances_list]
